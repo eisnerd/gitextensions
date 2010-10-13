7485f8da5cc8075083af9d20915383e22eb0785d
Davide <David.Eisner@trinitysystems.com>
2010-10-13 16:57:06 +0100

    No x64, installer

--- SimpleExt/StdAfx.h
+++ SimpleExt/StdAfx.h
@@ -10,8 +10,8 @@
 #endif // _MSC_VER > 1000
 
 #define STRICT
-#define WINVER          0x0400
-#define _WIN32_WINNT    0x0400
+#define WINVER          0x0501
+#define _WIN32_WINNT    0x0501
 #define _WIN32_IE       0x0501
 
 #define _ATL_APARTMENT_THREADED
